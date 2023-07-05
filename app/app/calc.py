7c8605c3ecb0913ec310ffec8072e551bd4b6072
virajsisodiya <sisodiyaviraj12@gmail.com>
2023-07-05 17:29:46 +0530

    Solved Lint Errors

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,10 +2,11 @@
 Calculator functions
 """
 
-def add(x,y):
+
+def add(x, y):
     "Add x and y and return result"
     return x+y
 
-def subtract(x,y):
+def subtract(x, y):
     "subtract y from x"
     return y-x
